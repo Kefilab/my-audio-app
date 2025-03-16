1bd6cc377e56587d40015fe59c6bbbb19310c421
Bal/ <bal@banatec.com>
2025-03-16 00:02:43 +0000

    updating latest files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,16 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "myaudioappa6bcabe6": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    },
-    "userPoolGroups": {
-      "AudioAppGroupRole": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
